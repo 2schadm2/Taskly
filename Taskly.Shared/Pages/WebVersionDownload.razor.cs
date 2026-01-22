ea0eda2cf7c26fc281b409de8a1dcc40c7d80dc5
Sudip Chand <sdpchand@gmail.com>
2026-01-22 13:26:50 +0545

    Login complere commit

--- Taskly.Shared/Pages/WebVersionDownload.razor.cs
+++ Taskly.Shared/Pages/WebVersionDownload.razor.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace Taskly.Shared.Pages
 {
-    public partial class VersionDownload
+    public partial class WebVersionDownload
     {
         [Inject]
         public NavigationManager NavigationManager { get; set; }
